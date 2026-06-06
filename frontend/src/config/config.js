c10421f2315ef35a2f3f085d762602b8a4e59df6
Ninad Vekar <ninad04d@gmail.com>
2026-06-07 00:35:45 +0530

    Dockerize MyJobFinder application

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,8 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+// const API_BASE_URL = "http://127.0.0.1:8000";
+
+// export default API_BASE_URL;
+
+const API_BASE_URL =
+  "http://localhost:8000";
 
 export default API_BASE_URL;
